3e1f35643d40c735db9cce5cc5edd68ec343b7d4
Mariya-Lisachenko <paramarabara@mail.ru>
2022-12-13 16:12:28 +0500

    Design

---
+++ Pages/NewRecipe.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FUNTIK.Pages
+{
+    public class NewRecipeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
