0d27e9297981fd12c29bb36f93100b96637fbdf0
TashK101 <92291989+TashK101@users.noreply.github.com>
2022-12-15 00:24:52 +0500

    Update NewRecipe.cshtml.cs

--- Pages/NewRecipe.cshtml.cs
+++ Pages/NewRecipe.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using FUNTIK.Models;
+using FUNTIK.GRASP;
 
 namespace FUNTIK.Pages
 {
     public class NewRecipeModel : PageModel
     {
+        public string Message { get; private set; } = "";
+        public RecipeMaker RecipeMaker = new RecipeMaker();
+        
+        public void OnPost(int mass, int sugarpercent, int milkpercent, int fatpercent, int cocoapercent)
+        {
+            //RecipeMaker = new RecipeMaker();
+            RecipeMaker.UpdateRecipeBase(mass, cocoapercent, fatpercent, sugarpercent, milkpercent);
+            Message = $"Recipe {RecipeMaker.Recipe.Mass} {RecipeMaker.Recipe.FatPercent} {RecipeMaker.Recipe.CacaoPercent}";
+        }
+
         public void OnGet()
         {
+            Message = $"Recipe {RecipeMaker.Recipe.Mass} {RecipeMaker.Recipe.FatPercent} {RecipeMaker.Recipe.CacaoPercent}";
         }
     }
 }
