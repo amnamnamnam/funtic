6ec366a8dc2348b0a87f5bed0a39a859dae8e425
TashK101 <92291989+TashK101@users.noreply.github.com>
2022-12-15 00:31:28 +0500

    Add files via upload

---
+++ Migrations/20221214151314_RecipeUpd.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FUNTIK.Migrations
+{
+    /// <inheritdoc />
+    public partial class RecipeUpd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CacaoPercent",
+                table: "Recipes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "FatPercent",
+                table: "Recipes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CacaoPercent",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "FatPercent",
+                table: "Recipes");
+        }
+    }
+}
---
+++ Migrations/20221214180308_RecipeUpd2.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FUNTIK.Migrations
+{
+    /// <inheritdoc />
+    public partial class RecipeUpd2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Mass",
+                table: "Recipes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MilkPersent",
+                table: "Recipes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SugarPercent",
+                table: "Recipes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Mass",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "MilkPersent",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "SugarPercent",
+                table: "Recipes");
+        }
+    }
+}
