ea9d9c738ea7e17ee42632f7d36a139a5c747cd9
Alex <nlxn@yandex.ru>
2022-12-20 14:36:37 +0500

    Add javascript checker

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function toggleButton() {
+    const [mass, sugarpercent, milkpercent, fatpercent, cocoapercent] = [
+        'mass', 'sugarpercent', 'milkpercent', 'fatpercent', 'cocoapercent'
+    ].map(x => document.getElementsByName(x)[0]);
+
+    const send_btn = document.getElementById('send');
+    const commonPercent = parseInt(sugarpercent.value) + parseInt(milkpercent.value) + parseInt(fatpercent.value) + parseInt(cocoapercent.value);
+    console.log('before if', commonPercent);
+    if (commonPercent === 100) {
+        send_btn.disabled = false;
+        console.log('button unlock');
+    } else {
+        send_btn.disabled = true;
+        console.log('button lock');
+    }
+}
